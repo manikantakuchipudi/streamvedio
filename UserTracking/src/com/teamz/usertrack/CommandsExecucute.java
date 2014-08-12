b10236e461a084aea87287a4dc518ae07f17b24e
manikanta <manikanta999.kuchipudi@gmail.com>
2014-08-12 08:46:06 -0700

    UserTrack from file 8/12/2014

---
+++ UserTracking/src/com/teamz/usertrack/CommandsExecucute.java
@@ -0,0 +1,24 @@
+package com.teamz.usertrack;
+
+
+public class CommandsExecucute {
+	
+	
+	
+	public void CommandsExecute(String command,String CompanyName)
+	{
+		switch (command) {
+		case "":
+			
+			break;
+
+		default:
+			break;
+		}
+		
+		
+	}
+	
+	
+
+}
---
+++ UserTracking/src/com/teamz/usertrack/CompanysFileReader.java
@@ -0,0 +1,53 @@
+package com.teamz.usertrack;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.Vector;
+
+public class CompanysFileReader {
+	
+	private Vector<String> companysList=null;
+	
+	
+	
+	public Vector<String> fileReader(String filename)
+	{
+		companysList=new Vector<String>();
+		BufferedReader br=null;
+		String stline=null;
+		try
+		{
+			if(filename==null)
+			br=new BufferedReader(new FileReader("CompanyNamex.txt"));
+			else
+				br=new BufferedReader(new FileReader(filename));
+			while((stline=br.readLine())!=null)
+			{
+				companysList.add(stline);
+			}
+		}
+		catch(IOException e)
+		{
+			
+		}
+		finally
+		{
+		try
+		{
+			if(br!=null)
+			{
+				br.close();
+				br=null;
+			}
+		}
+		catch(IOException ioe)
+		{
+			
+		}
+		
+	}
+		return companysList;
+	}
+}
